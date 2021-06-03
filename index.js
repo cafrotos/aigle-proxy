a8678235e4277876807d390b8c69f487738e3a6a
Cafrotos <nguyen.minhphuong3123@gmail.com>
2021-06-03 23:25:12 +0700

    test force ssl

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = require('express')();
- 
+
+var forceSsl = function (req, res, next) {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    return res.redirect(['https://', req.get('Host'), req.url].join(''));
+  }
+  return next();
+};
+
+app.configure(function () {
+  app.use(forceSsl);
+});
+
 app.use('/', createProxyMiddleware({ target: 'http://aigle.blife.ai', changeOrigin: true }));
 
 app.listen(process.env.PORT || 3000)
